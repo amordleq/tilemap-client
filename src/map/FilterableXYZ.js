d801950628709d30c3c30a604ba8477ffc1777d7
Russell Parry <amordleq@users.noreply.github.com>
2020-04-22 05:23:28 -0400

    make layer type part of the url

--- src/map/FilterableXYZ.js
+++ src/map/FilterableXYZ.js
@@ -16,7 +16,7 @@ class FilterableXYZ extends XYZ {
     }
 
     composeTileUrl(coordinates) {
-        const url = `/${coordinates[0]}/${coordinates[1]}/${coordinates[2]}.png`
+        const url = `/heatmap/${coordinates[0]}/${coordinates[1]}/${coordinates[2]}.png`
         if (this.filter) {
             return `${url}?filter=${encodeURIComponent(JSON.stringify(this.filter))}`
         } else {
